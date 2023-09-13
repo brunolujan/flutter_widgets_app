c244abecf646d7a45939a697f1528116438a6205
Bruno Luján <bruno1529@live.com.mx>
2023-09-13 13:01:18 -0600

    Menu items implementation and App theme modification

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -20,5 +20,12 @@ class AppTheme {
   }): assert(selectedColor >= 0, 'Selected color must be greater than 0'),
       assert(selectedColor < colorList.length, 'Selected color must be less or equal than ${colorList.length - 1}');
 
-  ThemeData getTheme() => ThemeData(useMaterial3: true, colorSchemeSeed: colorList[selectedColor]);
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true, 
+    colorSchemeSeed: 
+    colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: false
+    )
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,13 +14,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 8).getTheme(),
-      home: Scaffold(
-        body: Center(
-          child: FilledButton(child: Text('Hola'), onPressed: () {
-            
-          },)
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
