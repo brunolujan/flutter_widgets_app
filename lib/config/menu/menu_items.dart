503247f19c03948fe40cc59548ace46f30140315
Bruno Luján <bruno1529@live.com.mx>
2023-09-18 18:36:54 -0600

    Snackbars and Dialog Screen finished

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -26,13 +26,20 @@ const appMenuItems = <MenuItem>[
     title: 'Cards', 
     subTitle: 'A stylized container', 
     link: '/cards', 
-    icon: Icons.aspect_ratio_rounded
+    icon: Icons.analytics_rounded
   ),
 
     MenuItem(
-    title: 'Progress Indicator', 
+    title: 'Progress Indicators', 
     subTitle: 'Generals and controlled', 
     link: '/progress', 
-    icon: Icons.refresh_rounded
+    icon: Icons.replay_circle_filled_outlined
+  ),
+
+    MenuItem(
+    title: 'Snack Bars and Dialog Window', 
+    subTitle: 'Indicators on screen', 
+    link: '/snackbars', 
+    icon: Icons.info_rounded
   ),
 ];
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,12 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackbar = SnackBar(
+      content: const Text('El pepe'),
+      action: SnackBarAction(label: 'Mamalón!', onPressed: () {}),
+      duration: const Duration(seconds: 1),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
+  void openDialog(BuildContext context){
+    showDialog(
+      barrierDismissible: false,
+      context: context, 
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure?'),
+        content: const Text('Aliqua labore laboris mollit excepteur. Et fugiat veniam tempor pariatur elit quis eu ea aute sit qui. Qui ut culpa cillum deserunt ea excepteur ad cupidatat tempor qui. Voluptate ipsum labore voluptate dolor. Mollit consequat proident sit aliquip nisi cupidatat eiusmod do ad quis culpa minim ad.'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(), 
+            child: const Text('Cancel'),
+          ),
+          FilledButton(
+            onPressed: () => context.pop(), 
+            child: const Text('Acept'),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final colors = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        titleTextStyle: TextStyle(color: colors.background, fontSize: 21),
+        backgroundColor: colors.secondary,
+        title: const Text('Snackbars and Dialog Screen'),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  applicationVersion: 'V0.1',
+                  applicationIcon: const Icon(Icons.info),
+                  context: context,
+                  children: [
+                    const Text('In this dialog screen, you are allowed to see which and what kind of licenses I am using to develop this application')   
+                  ]
+                );
+              }, 
+              child: const Text('Used licenses')),
+              
+            FilledButton.tonal(
+              onPressed: () => openDialog(context), 
+              child: const Text('Show dialog'))
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Show Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_rounded),
+        onPressed: () => showCustomSnackbar(context),
+      ),
+    );
   }
 }
